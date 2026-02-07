258223fde4f5a62be4af5925608592acbdc36b18
Misaka-12450 <8287084+Misaka-12450@users.noreply.github.com>
2026-02-07 17:59:39 +0700

    Feat: Add C1B2 load group class for 15 A socket-outlets and maximum demand calculation

--- src/as_nzs_3000/c1_maximum_demand.py
+++ src/as_nzs_3000/c1_maximum_demand.py
@@ -193,6 +193,34 @@ class C1B1(C1LoadGroup):
         if self.num <= 20:
             return 10
         return 10 + 5 * (self.num // 20)
+
+
+class C1B2(C1LoadGroup):
+    """
+    Where the electrical installation includes one or
+    more 15 A socket-outlets, other than socket-outlets
+    provided to supply electrical equipment set out in
+    load groups (c), (d), (e), (f), (g) and (l)(8, 10)
+    """
+
+    load_group = ["b", 2]
+    load_group_description = (
+        "Where the electrical installation includes one or "
+        "more 15 A socket-outlets, other than socket-outlets "
+        "provided to supply electrical equipment set out in "
+        "load groups (c), (d), (e), (f), (g) and (l)"
+    )
+    rule_1_unit_per_phase = (
+        "15 A for 1 to 20 points + 7.5 A "
+        "for each additional 20 points "
+        "or part thereof"
+    )
+    notes = [c1_notes.C1Note8, c1_notes.C1Note10]
+
+    def get_maximum_demand_a(self) -> float:
+        if self.num <= 20:
+            return 15
+        return 15 + 7.5 * (self.num // 20)
 class C1H(C1LoadGroup):
     """
     Not applicable
@@ -206,3 +234,4 @@ class C1H(C1LoadGroup):
 
 # Resolve forward references
 C1A1.load_group_exception = [C1A2, C1H]
+C1B2.load_group_exception = [C1C, C1D, C1E, C1F, C1G, C1L]
