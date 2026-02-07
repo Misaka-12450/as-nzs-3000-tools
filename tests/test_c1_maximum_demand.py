ccdbe18d31867b049101103ed9f8d90c27c6b862
Misaka-12450 <8287084+Misaka-12450@users.noreply.github.com>
2026-02-08 01:47:15 +0700

    Fix: Update C1A1 and C1A2 test cases to use new parameter names

--- tests/test_c1_maximum_demand.py
+++ tests/test_c1_maximum_demand.py
@@ -5,28 +5,28 @@ from src.as_nzs_3000.c1_maximum_demand import C1A1, C1A2, IncorrectLoadGroupExce
 
 class TestC1A1:
     def test_max_demand_1_to_20_points(self):
-        load = C1A1(num=20, rating_w=10)
-        assert load.get_maximum_demand_a() == 3
+        load = C1A1(rating=10, num_load=20)
+        assert load.maximum_demand_a == 3
 
     def test_max_demand_21_points(self):
-        load = C1A1(num=21, rating_w=10)
-        assert load.get_maximum_demand_a() == 5
+        load = C1A1(rating=10, num_load=21, rating_type="W")
+        assert load.maximum_demand_a == 5
 
     def test_max_demand_40_points(self):
-        load = C1A1(num=40, rating_w=10)
-        assert load.get_maximum_demand_a() == 7
+        load = C1A1(rating=10, num_load=40, rating_type="W")
+        assert load.maximum_demand_a == 7
 
     def test_str(self):
-        load = C1A1(num=21, rating_w=10)
+        load = C1A1(rating=10, num_load=20, rating_type="W")
         print(str(load))
 
 
 class TestC1A2:
     def test_str(self):
-        load = C1A2(num=21, rating_w=50)
+        load = C1A2(rating=50, num_load=21, rating_type="W")
         print(str(load))
 
     def test_under_1000w_raises_exception(self):
         with pytest.raises(IncorrectLoadGroupException) as exc_info:
-            C1A2(num=21, rating_w=10)
+            C1A2(rating=10, num_load=21, rating_type="W")
         assert exc_info.value.correct_group is C1A1
