5e5db95f5c81580e6a4fba20241b1ce505f85946
Misaka-12450 <8287084+Misaka-12450@users.noreply.github.com>
2026-02-07 17:23:48 +0700

    Test: Add unit tests for C1A1 and C1A2 maximum demand calculations

---
+++ tests/test_c1_maximum_demand.py
@@ -0,0 +1,32 @@
+import pytest
+
+from src.as_nzs_3000.c1_maximum_demand import C1A1, C1A2, IncorrectLoadGroupException
+
+
+class TestC1A1:
+    def test_max_demand_1_to_20_points(self):
+        load = C1A1(num=20, rating_w=10)
+        assert load.get_maximum_demand_a() == 3
+
+    def test_max_demand_21_points(self):
+        load = C1A1(num=21, rating_w=10)
+        assert load.get_maximum_demand_a() == 5
+
+    def test_max_demand_40_points(self):
+        load = C1A1(num=40, rating_w=10)
+        assert load.get_maximum_demand_a() == 7
+
+    def test_str(self):
+        load = C1A1(num=21, rating_w=10)
+        print(str(load))
+
+
+class TestC1A2:
+    def test_str(self):
+        load = C1A2(num=21, rating_w=50)
+        print(str(load))
+
+    def test_under_1000w_raises_exception(self):
+        with pytest.raises(IncorrectLoadGroupException) as exc_info:
+            C1A2(num=21, rating_w=10)
+        assert exc_info.value.correct_group is C1A1
